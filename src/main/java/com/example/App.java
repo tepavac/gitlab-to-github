2594a218c0234591ced43241f9cd473b92706954
Milos Tepavac <milos.tepavac@m2.com>
2025-04-30 10:54:29 +0400

    Add some secrets in the code.

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,6 +1,7 @@
 package com.example;
 
 public class App {
+    private static String apiKey = "sk_test_1234567890abcdef";
     public static void main(String[] args) {
         System.out.println("Hello, GitHub Actions!");
     }
